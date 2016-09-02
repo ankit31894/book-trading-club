81c29b6153eedc7ab70218a684b1d3d918b689d0
Ankit <ankit31894@gmail.com>
2016-09-02 08:57:56 +0530

    Changed google callback url

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '719744220001-s8020j9jnvl829tapvk9b1ssoirr1gff.apps.googleusercontent.com',
         'clientSecret'  : 'oqvSKSojpbCchOFARYe2Aw0b',
-        'callbackURL'   : 'http://127.0.0.1:8080/login/google/callback'
+        'callbackURL'   : 'https://ankit31894-btc.herokuapp.com/login/google/callback'
     }
 
 };
